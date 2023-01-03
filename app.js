7cd6861b721003522819baefcd219ac684c0b005
Saipraveen <praveensai18@gmail.com>
2023-01-03 12:03:13 +0000

    cricket solution

--- app.js
+++ app.js
@@ -27,12 +27,13 @@ const initializeDBAndServer = async () => {
 initializeDBAndServer();
 
 const convertDbObjectToResponseObject = (dbObject) => {
-  return {
+  let respObj = {
     playerId: dbObject.player_id,
     playerName: dbObject.player_name,
     jerseyNumber: dbObject.jersey_number,
     role: dbObject.role,
   };
+  return respObj;
 };
 
 // Get Players API
@@ -55,20 +56,20 @@ app.get("/players/", async (request, response) => {
 app.post("/players/", async (request, response) => {
   const playerDetails = request.body;
   const { playerName, jerseyNumber, role } = playerDetails;
+  console.log(playerDetails);
   const addPlayerQuery = `
     INSERT INTO
       cricket_team (player_name, jersey_number, role)
     VALUES
       (
-        'Vishal',
-        17,
-        'Bowler'
+        "${playerName}",
+        ${jerseyNumber},
+        "${role}"
       );`;
 
   const dbResponse = await db.run(addPlayerQuery);
   const playerId = dbResponse.lastID;
-  console.log(dbResponse);
-  response.send("Player Added To Team");
+  response.send("Player Added to Team");
 });
 
 //Get Player API
@@ -77,7 +78,6 @@ app.get("/players/:playerId/", async (request, response) => {
   const getPlayerQuery = `SELECT * FROM cricket_team WHERE player_id = ${playerId};`;
   const player = await db.get(getPlayerQuery);
   responseObjectOfPlayer = convertDbObjectToResponseObject(player);
-  console.log(typeof player);
   response.send(responseObjectOfPlayer);
 });
 
@@ -90,18 +90,18 @@ app.put("/players/:playerId/", async (request, response) => {
     UPDATE
       cricket_team
     SET
-      player_name='Maneesh',
-      jersey_number=54,
-      role='All-rounder'
+      player_name='${playerName}',
+      jersey_number=${jerseyNumber},
+      role='${role}'
     WHERE
-      player_id='${playerId}';`;
+      player_id = ${playerId};`;
 
   await db.run(updatePlayerQuery);
   response.send("Player Details Updated");
 });
 
 // Delete Player API
-app.delete("/players/:playerId", async (request, response) => {
+app.delete("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const deletePlayerQuery = `
     DELETE FROM
@@ -112,4 +112,4 @@ app.delete("/players/:playerId", async (request, response) => {
   response.send("Player Removed");
 });
 
-module.exports = express;
+module.exports = app;
